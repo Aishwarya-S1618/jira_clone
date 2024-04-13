a850205023858d37fb52a299409c95f39b6a7ec5
Aishwarya-S1618 <saishwarya1618@gmail.com>
2024-04-13 15:03:33 +0530

    date commit

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>Welcome to SprintCrew</div>
+      <p>Date: 3:00pm 12th April 2024</p>
     </main>
   );
 }
